df0f0628acd7c857680461634c53f96ed3b2bdad
Richard Quigley <rquigley@taylorhopkinson.com>
2023-04-28 12:47:58 +0100

    [ADD] Favicon folder to public and links

--- pages/_document.js
+++ pages/_document.js
@@ -5,7 +5,26 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="en" className="theme-compiled">
-        <Head />
+        <Head>
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="favicon/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="favicon/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="favicon/favicon-16x16.png"
+          />
+          <link rel="manifest" href="favicon/site.webmanifest" />
+        </Head>
         <body
           className={`antialiased text-lg bg-white dark:bg-gray-900 dark:text-white leading-base`}
         >
